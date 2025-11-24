c565ac3a113080fdabab0be7bae7822c719d10e0
soulayman <soulaymanjaa09@gmail.com>
2025-11-24 14:29:40 +0100

    made an image for logo and styled it

--- src/componants/comon/Header.tsx
+++ src/componants/comon/Header.tsx
@@ -7,15 +7,14 @@ export default function Header() {
    return (
     <header className='flex border-b border-gray-300 py-3 px-4 sm:px-10 bg-white min-h-[65px] tracking-wide relative z-50'>
       <div className='flex flex-wrap items-center gap-4 max-w-screen-xl mx-auto w-full'>
-        <a href="javascript:void(0)" className="max-sm:hidden"><img src="https://readymadeui.com/readymadeui.svg" alt="logo" className='w-[134px]' />
+        <a href="javascript:void(0)" id="logo" className="max-sm:hidden"><img src="/src/assets/QSXLogo.png" alt="logo" className='w-[134px]' />
         </a>
-        <a href="javascript:void(0)" className="hidden max-sm:block"><img src="https://readymadeui.com/readymadeui-short.svg" alt="logo" className='w-8' />
-        </a>
+        
 
         <div id="collapseMenu"
           className='max-lg:hidden lg:!block max-lg:w-full max-lg:fixed max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50'>
           <button id="toggleClose" className='lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white w-9 h-9 flex items-center justify-center border border-gray-200 cursor-pointer'>
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-3.5 h-3.5 fill-slate-900" viewBox="0 0 320.591 320.591">
+            <svg xmlns="/src/assets/QSXLogo.png" className="w-3.5 h-3.5 fill-slate-900" viewBox="0 0 320.591 320.591">
               <path
                 d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
                 data-original="#000000"></path>
@@ -28,7 +27,7 @@ export default function Header() {
           <ul
             className='lg:flex lg:ml-14 lg:gap-x-5 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50'>
             <li className='mb-6 hidden max-lg:block'>
-              <a href="javascript:void(0)"><img src="https://readymadeui.com/readymadeui.svg" alt="logo" className='w-36' />
+              <a href="javascript:void(0)"><img src="" alt="logo" className='w-36' />
               </a>
             </li>
             <li className='max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3'>
