44ece4d93192cd5ffcf7507132558efa1daad140
soulayman <soulaymanjaa09@gmail.com>
2025-11-24 14:13:32 +0100

    made header componant

---
+++ src/views/Dashboard.tsx
@@ -0,0 +1,13 @@
+// import React from "react";
+import Header from "../componants/comon/Header";
+
+
+export default function Dashboard() {
+
+  return (
+   <main>
+    
+    <Header></Header>
+   </main>
+  );
+}
