6db601c186ec9f545d296d329ccc45ba33fa8340
soulayman <soulaymanjaa09@gmail.com>
2025-11-28 12:16:29 +0100

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Dashboard from "./views/Dashboard";
+import MapPage from "./views/MapPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
       <Route index element={<Dashboard />}/>
+      <Route path="/map" element={<MapPage />}/>
       </Route>
     </Routes>
   );
---
+++ src/views/MapPage.tsx
@@ -0,0 +1,18 @@
+import Map from "@/components/map/Map"
+
+
+
+
+export default function MapPage() {
+  return (
+    <section>
+           <h2>Map here</h2>
+      <div>
+        <Map>
+
+        </Map>
+      </div>
+
+    </section>
+  )
+}
